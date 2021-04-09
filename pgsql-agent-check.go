0a25c207ccabc9a475b8d8bc8e59552358e019ce
mmayen <mmayen@haproxy.com>
2021-04-09 13:50:55 +0200

    Add some log and now the verbosity can be set

--- pgsql-agent-check.go
+++ pgsql-agent-check.go
@@ -43,11 +43,13 @@ const DEFAULT_PORT string = "5432"
 const DEFAULT_LOG_FILE string = "pgsql-agent-check.log"
 const DEFAULT_WORKER string = "2"
 const DEFAULT_DELIMITER string = "|"
+const DEFAULT_LOG_LEVEL string = "1"
 const HELP string = `Help :
 --address  The agent listens on this address (default %s)
 --port     The agent listens on this port (default %s)
 --worker   The agent runs N worker (default %s)
 --logfile  The agent writes its log to this file (default %s)
+--loglevel The log verbosity level (default %s)
 `
 
 type PgsqlConnectionParameters struct {
@@ -56,7 +58,7 @@ type PgsqlConnectionParameters struct {
 	Username string
 	Password string
 	DbName   string
-	logChan  chan error
+	logChan  chan *LogMessage
 	dbConn   *sql.DB
 	reason   string
 }
@@ -65,12 +67,28 @@ func (pcp *PgsqlConnectionParameters) String() string {
 	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", pcp.Host, pcp.Port, pcp.Username, pcp.Password, pcp.DbName)
 }
 
+type LogMessage struct {
+	Message  error
+	LogLevel int
+}
+
+func (lm *LogMessage) Error() string {
+	return lm.Message.Error()
+}
+func NewLogMessage(err error, logLevel int) *LogMessage {
+	var lm *LogMessage = new(LogMessage)
+	lm.Message = err
+	lm.LogLevel = logLevel
+	return lm
+}
+
 type AgentParameters struct {
 	address        string
 	port           string
 	logFile        string
+	logLevel       string
 	worker         string
-	logChan        chan error
+	logChan        chan *LogMessage
 	wgWorker       *sync.WaitGroup
 	wgServer       *sync.WaitGroup
 	wgLogger       *sync.WaitGroup
@@ -86,13 +104,13 @@ func (ap *AgentParameters) GetTCPAddr() *net.TCPAddr {
 	var address net.IP
 	var err error
 	if err = address.UnmarshalText([]byte(ap.address)); err != nil {
-		ap.logChan <- err
-		ap.logChan <- errors.New("Fallback to the default address (" + DEFAULT_ADDRESS + ").")
+		ap.logChan <- NewLogMessage(err, 1)
+		ap.logChan <- NewLogMessage(errors.New("Fallback to the default address ("+DEFAULT_ADDRESS+")."), 1)
 		address.UnmarshalText([]byte(DEFAULT_ADDRESS))
 	}
 	if port, err = strconv.Atoi(ap.port); err != nil {
-		ap.logChan <- err
-		ap.logChan <- errors.New("Fallback to the default address (" + DEFAULT_PORT + ").")
+		ap.logChan <- NewLogMessage(err, 1)
+		ap.logChan <- NewLogMessage(errors.New("Fallback to the default address ("+DEFAULT_PORT+")."), 1)
 		port, _ = strconv.Atoi(DEFAULT_PORT)
 	}
 	return &net.TCPAddr{
@@ -104,8 +122,8 @@ func (ap *AgentParameters) GetNbWorker() int {
 	var nbWorker int
 	var err error
 	if nbWorker, err = strconv.Atoi(ap.worker); err != nil {
-		ap.logChan <- err
-		ap.logChan <- errors.New("Fallback to the default number of worker(" + DEFAULT_WORKER + ").")
+		ap.logChan <- NewLogMessage(err, 1)
+		ap.logChan <- NewLogMessage(errors.New("Fallback to the default number of worker("+DEFAULT_WORKER+")."), 1)
 		nbWorker, _ = strconv.Atoi(DEFAULT_WORKER)
 	}
 	return nbWorker
@@ -115,8 +133,9 @@ func NewAgentParameters() *AgentParameters {
 	ap.address = DEFAULT_ADDRESS
 	ap.port = DEFAULT_PORT
 	ap.logFile = DEFAULT_LOG_FILE
+	ap.logLevel = DEFAULT_LOG_LEVEL
 	ap.worker = DEFAULT_WORKER
-	ap.logChan = make(chan error)
+	ap.logChan = make(chan *LogMessage)
 	ap.wgWorker = new(sync.WaitGroup)
 	ap.wgServer = new(sync.WaitGroup)
 	ap.wgLogger = new(sync.WaitGroup)
@@ -131,7 +150,7 @@ func main() {
 	var sigs chan os.Signal = make(chan os.Signal, 1)
 	var agentParameters *AgentParameters = ParseArgs()
 	agentParameters.wgLogger.Add(1)
-	go LogError(agentParameters)
+	go Logger(agentParameters)
 	for i := 0; i < agentParameters.GetNbWorker(); i++ {
 		agentParameters.wgWorker.Add(1)
 		go Worker(agentParameters, i)
@@ -142,17 +161,17 @@ func main() {
 	var sig os.Signal = <-sigs
 	switch sig {
 	case syscall.SIGUSR1:
-		agentParameters.logChan <- errors.New("HAProxy reloads.")
+		agentParameters.logChan <- NewLogMessage(errors.New("HAProxy reloads."), 0)
 		agentParameters.listener.Close()
 		agentParameters.wgKiller.Add(1)
 		StopAllRoutines(agentParameters)
 	case syscall.SIGTERM:
-		agentParameters.logChan <- errors.New("HAProxy stops/restarts.")
+		agentParameters.logChan <- NewLogMessage(errors.New("HAProxy stops/restarts."), 0)
 		agentParameters.listener.Close()
 		agentParameters.wgKiller.Add(1)
 		StopAllRoutines(agentParameters)
 	default:
-		agentParameters.logChan <- errors.New(fmt.Sprintf("Received %s", fmt.Sprint(sig)))
+		agentParameters.logChan <- NewLogMessage(errors.New(fmt.Sprintf("Received %s", fmt.Sprint(sig))), 0)
 		fmt.Println(sig)
 		agentParameters.listener.Close()
 		agentParameters.wgKiller.Add(1)
@@ -183,8 +202,11 @@ func ParseArgs() *AgentParameters {
 			case "--logfile":
 				setting = &agentParameters.logFile
 				waitValue = true
+			case "--loglevel":
+				setting = &agentParameters.logLevel
+				waitValue = true
 			case "--help":
-				fmt.Printf(HELP, DEFAULT_ADDRESS, DEFAULT_PORT, DEFAULT_WORKER, DEFAULT_LOG_FILE)
+				fmt.Printf(HELP, DEFAULT_ADDRESS, DEFAULT_PORT, DEFAULT_WORKER, DEFAULT_LOG_FILE, DEFAULT_LOG_LEVEL)
 				os.Exit(0)
 			}
 		}
@@ -200,29 +222,30 @@ func Server(ap *AgentParameters) {
 		for loop := true; loop; {
 			if conn, err = ap.listener.AcceptTCP(); err == nil {
 				ap.connChan <- conn
+				ap.logChan <- NewLogMessage(errors.New(fmt.Sprintf("New connection accepted from %s", conn.RemoteAddr().String())), 2)
 			} else {
 				switch err.(type) {
 				case *net.OpError:
 					if err.(*net.OpError).Err.Error() == "use of closed network connection" {
 						loop = false
-						ap.logChan <- errors.New("The listener has been closed.")
+						ap.logChan <- NewLogMessage(errors.New("The listener has been closed."), 1)
 						break
 					} else {
-						ap.logChan <- err
+						ap.logChan <- NewLogMessage(err, 1)
 					}
 				default:
-					ap.logChan <- err
+					ap.logChan <- NewLogMessage(err, 1)
 				}
 			}
 		}
 	} else {
-		ap.logChan <- err
+		ap.logChan <- NewLogMessage(err, 1)
 		return
 	}
 }
 func Worker(ap *AgentParameters, i int) {
 	defer ap.wgWorker.Done()
-	ap.logChan <- errors.New(fmt.Sprintf("The worker %d starts.", i))
+	ap.logChan <- NewLogMessage(errors.New(fmt.Sprintf("The worker %d starts.", i)), 0)
 	for loop := true; loop; {
 		select {
 		case conn := <-ap.connChan:
@@ -231,44 +254,46 @@ func Worker(ap *AgentParameters, i int) {
 			var err error
 			buff, err = reader.ReadBytes(10)
 			var pgParams *PgsqlConnectionParameters = ParseHAProxyRequest(ap, buff)
+			ap.logChan <- NewLogMessage(errors.New(fmt.Sprintf("Checking (h=%s,p=%d,db=%s,u=%s) for %s", pgParams.Host, pgParams.Port, pgParams.DbName, pgParams.Username, conn.RemoteAddr().String())), 3)
 
 			if GetDbConnection(pgParams); pgParams.dbConn == nil {
 				if _, err = conn.Write([]byte("DOWN DOWN#NetworkError\n")); err != nil {
-					ap.logChan <- err
+					ap.logChan <- NewLogMessage(err, 1)
 				}
 				pgParams.dbConn.Close()
 				break
 			}
 			if !CheckConnection(pgParams) {
 				if _, err = conn.Write([]byte(fmt.Sprintf("DOWN DOWN#%s\n", pgParams.reason))); err != nil {
-					ap.logChan <- err
+					ap.logChan <- NewLogMessage(err, 1)
 				}
 				pgParams.dbConn.Close()
 				break
 			}
 			if !CheckIsMaster(pgParams) {
 				if _, err = conn.Write([]byte("DOWN DOWN#NodeIsInRecovery\n")); err != nil {
-					ap.logChan <- err
+					ap.logChan <- NewLogMessage(err, 1)
 				}
 				pgParams.dbConn.Close()
 				break
 			}
 			if _, err = conn.Write([]byte("UP UP#NodeIsMAster\n")); err != nil {
-				ap.logChan <- err
+				ap.logChan <- NewLogMessage(err, 1)
 			}
 			pgParams.dbConn.Close()
 			if err = conn.Close(); err != nil {
-				ap.logChan <- err
+				ap.logChan <- NewLogMessage(err, 1)
 			}
+			ap.logChan <- NewLogMessage(errors.New(fmt.Sprintf("Check done for %s", conn.RemoteAddr().String())), 3)
 		case <-ap.stopWorkerChan:
 			loop = false
 		}
 	}
-	ap.logChan <- errors.New(fmt.Sprintf("The worker %d stops.", i))
+	ap.logChan <- NewLogMessage(errors.New(fmt.Sprintf("The worker %d stops.", i)), 0)
 }
 func GetDbConnection(pcp *PgsqlConnectionParameters) *sql.DB {
 	if db, err := sql.Open("postgres", pcp.String()); err != nil {
-		pcp.logChan <- err
+		pcp.logChan <- NewLogMessage(err, 1)
 		return nil
 	} else {
 		pcp.dbConn = db
@@ -277,7 +302,7 @@ func GetDbConnection(pcp *PgsqlConnectionParameters) *sql.DB {
 }
 func CheckConnection(pcp *PgsqlConnectionParameters) bool {
 	if err := pcp.dbConn.Ping(); err != nil {
-		pcp.logChan <- err
+		pcp.logChan <- NewLogMessage(err, 1)
 		switch {
 		case strings.HasPrefix(err.Error(), "pq: password authentication failed for user"):
 			pcp.reason = "AuthFailed"
@@ -297,7 +322,7 @@ func CheckIsMaster(pcp *PgsqlConnectionParameters) bool {
 	var row *sql.Row = pcp.dbConn.QueryRow("select pg_is_in_recovery()")
 	var isSlave bool
 	if err := row.Scan(&isSlave); err != nil {
-		pcp.logChan <- err
+		pcp.logChan <- NewLogMessage(err, 1)
 		return false
 	} else {
 		return !isSlave
@@ -323,26 +348,35 @@ func ParseHAProxyRequest(ap *AgentParameters, req []byte) *PgsqlConnectionParame
 	}
 	return pcp
 }
-func LogError(ap *AgentParameters) {
+func Logger(ap *AgentParameters) {
 	defer ap.wgLogger.Done()
-	if fd, fdErr := os.OpenFile(ap.logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0775); fdErr == nil {
-		fd.WriteString(fmt.Sprintf("%s: Agent starts.\n", time.Now()))
-		fd.Close()
+	WriteLog(ap, NewLogMessage(errors.New("Agent starts."), 0))
+	var logLevel int
+	var err error
+	if logLevel, err = strconv.Atoi(ap.logLevel); err != nil {
+		WriteLog(ap, NewLogMessage(errors.New(fmt.Sprintf("Invalid log level specified (%s), fallback to the default (%s)", ap.logLevel, DEFAULT_LOG_LEVEL)), 0))
+		logLevel, _ = strconv.Atoi(DEFAULT_LOG_LEVEL)
+	} else {
+		WriteLog(ap, NewLogMessage(errors.New(fmt.Sprintf("Log level set to %s", ap.logLevel)), 0))
 	}
 	for loop := true; loop; {
 		select {
-		case err := <-ap.logChan:
-			if fd, fdErr := os.OpenFile(ap.logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0775); fdErr == nil {
-				fd.WriteString(fmt.Sprintf("%s: %s\n", time.Now(), err.Error()))
-				fd.Close()
+		case logMessage := <-ap.logChan:
+			if logMessage.LogLevel <= logLevel {
+				WriteLog(ap, logMessage)
 			}
 		case <-ap.stopLoggerChan:
 			loop = false
 		}
 	}
+	WriteLog(ap, NewLogMessage(errors.New("Agent stops."), 0))
+}
+func WriteLog(ap *AgentParameters, lm *LogMessage) {
 	if fd, fdErr := os.OpenFile(ap.logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0775); fdErr == nil {
-		fd.WriteString(fmt.Sprintf("%s: Agent stops.\n", time.Now()))
+		fd.WriteString(fmt.Sprintf("%s: %s\n", time.Now(), lm.Error()))
 		fd.Close()
+	} else {
+		fmt.Println(fdErr.Error())
 	}
 }
 func StopAllRoutines(ap *AgentParameters) {
